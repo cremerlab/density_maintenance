ae7496ec627fadb6892e53fe9ac53998167307ca
gchure <griffinchure@gmail.com>
2023-01-11 13:06:25 -0800

    Addition of first draft of EDA and comparison with mass spec data

--- code/processing/compile_proteomics_data.py
+++ code/processing/compile_proteomics_data.py
@@ -1,11 +1,19 @@
-#%% 
-import pandas as pd 
+# %%
+import pandas as pd
 
-files =['Mori2021', 'Soufi2015', 'Caglar2017', 'Belliveau2021']
-dfs = [pd.read_csv(f'../../data/source/{f}/{f}_processed.csv') for f in files]
+files = ['Mori2021', 'Soufi2015', 'Caglar2017', 'Belliveau2021']
+dfs = [pd.read_csv(
+    f'../../data/literature/{f}/{f}_processed.csv') for f in files]
 data = pd.concat(dfs, sort=False)
+
+# Categorize
+data['periplasm'] = False
+data.loc[(data['go_terms'].str.contains('GO:0042597'))
+         | (data['go_terms'].str.contains('GO:0005620')), 'periplasm'] = True
+
+# %%
 data.drop(columns=['b_number', 'cog_category', 'cog_desc', 'gene_product', 'annotation', 'dataset'],
-            inplace=True)
+          inplace=True)
 data.to_csv('../../data/compiled_mass_fractions.csv', index=False)
 
 # %%
