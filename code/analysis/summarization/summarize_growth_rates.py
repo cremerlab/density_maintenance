53869ec06a1888825ab038d8b764e67bd1359666
gchure <griffinchure@gmail.com>
2023-02-04 18:22:03 -0800

    Addition of total growth curves with new indexing scheme

--- code/analysis/summarization/summarize_growth_rates.py
+++ code/analysis/summarization/summarize_growth_rates.py
@@ -5,16 +5,14 @@ import scipy.stats
 import tqdm
 data = pd.read_csv(
     '../../../data/growth_curves/growth_measurements_processed.csv')
-
 summarized = pd.DataFrame([])
-
-for g, d in tqdm.tqdm(data.groupby(['strain', 'carbon_source',  'run_no', 'date'])):
+for g, d in tqdm.tqdm(data.groupby(['strain', 'carbon_source',  'overexpression', 'inducer_conc_ng_ml', 'run_idx'])):
     popt = scipy.stats.linregress(
         d['elapsed_time_hr'].values, np.log(d['od_600nm']))
     slope = popt[0]
     intercept = popt[1]
-    _df = pd.DataFrame([np.array([g[0], g[1], g[2], g[3], slope, np.exp(intercept)])],
-                       columns=['strain', 'carbon_source', 'run_no', 'date', 'growth_rate_hr', 'od_init'])
+    _df = pd.DataFrame([np.array([g[0], g[1], g[2], g[3], g[4], slope, np.exp(intercept)])],
+                       columns=['strain', 'carbon_source', 'overexpression', 'inducer_conc_ng_ml', 'run_idx', 'growth_rate_hr', 'od_init'])
     summarized = pd.concat([summarized, _df], sort=False)
 
 # %%
--- code/processing/growth_curves/processing.py
+++ code/processing/growth_curves/processing.py
@@ -6,16 +6,15 @@ data = pd.read_csv(
     '../../../data/growth_curves/growth_measurements_raw.csv')
 
 # Filter on OD bounds
-data = data[(data['od_600nm'] >= 0.04) & (data['od_600nm'] <= 0.4)]
+data = data[(data['od_600nm'] >= 0.04) & (data['od_600nm'] <= 0.5)]
 
 # convert clock time to elapsed time
 data['clock_time'] = pd.to_datetime(data['clock_time'].values)
 processed = []
-for g, d in data.groupby(['strain', 'carbon_source', 'date', 'run_no']):
+for g, d in data.groupby(['run_idx']):
     _d = d.copy()
     _d['elapsed_time_hr'] = (
         d['clock_time'] - d['clock_time'].min()).dt.total_seconds() / 3600
-
     if len(_d) >= 3:
         processed.append(_d)
 processed = pd.concat(processed, sort=False)
