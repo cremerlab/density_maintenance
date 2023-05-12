2712f5034e9f54ac68d87ffbfad2ad2cafa988c7
gchure <griffinchure@gmail.com>
2023-05-11 19:59:13 -0700

    Fixed figure generation script

--- code/visualization/Fig3_model_results.py
+++ code/visualization/Fig3_model_results.py
@@ -14,15 +14,16 @@ ms_data = pd.read_csv(
 size_data = pd.read_csv(
     '../../data/literature/collated_literature_size_data.csv')
 ppcs = pd.read_csv('../../data/mcmc/literature_model_params.csv')
-kdes = pd.read_csv('../../data/mcmc/literature_model_params_kde.csv')
+ppcs = pd.read_csv(
+    '../../data/mcmc/literature_model_params.csv')
 
+m1_ppcs = ppcs[(ppcs['volume_scale'] == 'linear_width')
+               & (ppcs['model'] == 'const_phi_mem')]
+m2_ppcs = ppcs[(ppcs['volume_scale'] == 'linear_width')
+               & (ppcs['model'] == 'const_rho_mem')]
 size_data['aspect_ratio'] = size_data['length_um'].values / \
     size_data['width_um'].values
-m2_ppcs = ppcs[(ppcs['model'] == 'const_rho_mem')]
-# ppcs = ppcs[ppcs['model'] == 'const_phi_mem']
-kdes = kdes[kdes['model'] == 'const_phi_mem']
 size_data['aspect_ratio'] = size_data['length_um'] / size_data['width_um']
-
 
 # %%
 
@@ -50,9 +51,9 @@ ax[3].set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
 
 
 # Isolate the median values for the constants
-const_medians = ppcs[(ppcs['interval'] == 'median') &
-                     (ppcs['quantity'].isin(['alpha_sim', 'm_peri_sim',
-                                             'phi_mem_sim']))]
+const_medians = m1_ppcs[(m1_ppcs['interval'] == 'median') &
+                        (m1_ppcs['quantity'].isin(['alpha_sim', 'm_peri_sim',
+                                                   'phi_mem_sim']))]
 const_rho_mem = m2_ppcs[(m2_ppcs['interval'] == 'median') &
                         (m2_ppcs['quantity'] == 'rho_mem_sim')]
 # Plot the literature data
@@ -85,53 +86,6 @@ ax[2].plot(const_rho_mem['growth_rate_hr'], const_rho_mem['lower'], '-',
 plt.subplots_adjust(hspace=0.35)
 plt.savefig('../../Fig3_constant_plots.pdf', bbox_inches='tight')
 
-
-# %%
-fig, ax = plt.subplots(1, 3, figsize=(3, 1))
-ax = ax.ravel()
-axes = {'m_peri_mu': ax[0], 'phi_mem_mu': ax[1],
-        'alpha': ax[2], 'rho_mem_mu': ax[3]}
-for a in ax:
-    a.set_yticks([])
-    a.set_facecolor('#FFF')
-ax[0].set_xlim([8, 12])
-ax[0].set_xticks([8, 10, 12])
-ax[1].set_xlim([0.10, 0.15])
-ax[1].set_xticks([0.10, 0.125, 0.15])
-ax[2].set_xlim([3.5, 4.5])
-ax[0].set_xlabel('$m_{peri}$ [fg / cell]', fontsize=6)
-ax[1].set_xlabel('$\phi_{mem}$', fontsize=6)
-ax[3].set_ylim([0, 5])
-
-# Plot the probability density
-percs = [(2.5, 97.5), (12.5, 87.5), (37.5, 62.5), (49.5, 50.5)]
-perc_colors = {'95%': 'pale',
-               '75%': 'light',
-               '25%': 'primary',
-               'median': 'dark'}
-for g, d in kdes[kdes['parameter'].isin(['m_peri_mu', 'phi_mem_mu', 'alpha', 'rho_mem_mu'])].groupby(['parameter']):
-    axes[g].fill_between(d['value'], np.zeros(len(d)), d['kde'] / d['kde'].max(),  color=cor['pale_blue'],
-                         alpha=0.5)
-    axes[g].plot(d['value'], d['kde'] / d['kde'].max(),
-                 '-', lw=0.5, color=cor['primary_blue'])
-
-for i, (g, d) in enumerate(m2_ppcs[m2_ppcs['quantity'].isin(['w_rep', 'ell_rep', 'vol_rep'])].groupby(['quantity', 'interval'], sort=False)):
-    if g[1] != 'median':
-        ax[i].fill_between(d['growth_rate_hr'], d['lower'], d['upper'],
-                           color=cor[f'{perc_colors[g[0]]}_green'], alpha=0.5)
-# # Plot the major percentiles
-# axes = {'m_peri_sim': ax[0], 'phi_mem_sim': ax[1], 'alpha_sim': ax[2]}
-# err_widths = {'95%': 0.5, '75%': 1, '25%': 1.5}
-# for g, d in ppcs[ppcs['quantity'].isin(['alpha_sim', 'phi_mem_sim', 'alpha_sim'])].groupby(['quantity', 'interval']):
-#     if g[1] in ['95%', '75%', '25%', 'median']:
-#         if g[1] != 'median':
-#             axes[g[0]].hlines(-0.05, d['lower'].values[0], d['upper'].values[0],
-#                               lw=err_widths[g[1]], color=cor['primary_blue'])
-
-# plt.tight_layout()
-plt.savefig('../../figures/Fig3_const_param_posteriors.pdf',
-            bbox_inches='tight')
-
 # %%
 fig, ax = plt.subplots(1, 4, figsize=(6, 1.5), sharex=True)
 for a in ax:
@@ -156,10 +110,9 @@ for g, d in size_data.groupby(['source']):
                    alpha=0.75, zorder=1000)
 axes = {'w_rep': ax[1], 'ell_rep': ax[2], 'vol_rep': ax[3], 'alpha_rep': ax[0]}
 
-_ppcs = ppcs[(ppcs['volume_scale'] == 'linear_width') &
-             (ppcs['model'] == 'const_rho_mem')]
-for g, d in _ppcs[_ppcs['quantity'].isin(['w_rep', 'ell_rep', 'vol_rep', 'alpha_rep']) &
-                  (_ppcs['interval'].isin(perc_colors.keys()))].groupby(['quantity', 'interval'], sort=False):
+
+for g, d in m1_ppcs[m1_ppcs['quantity'].isin(['w_rep', 'ell_rep', 'vol_rep', 'alpha_rep']) &
+                    (m1_ppcs['interval'].isin(perc_colors.keys()))].groupby(['quantity', 'interval'], sort=False):
     if g[1] != 'median':
         axes[g[0]].fill_between(d['growth_rate_hr'], d['lower'], d['upper'],
                                 color=cor[f'{perc_colors[g[1]]}_black'], alpha=0.25)
@@ -167,7 +120,7 @@ for g, d in _ppcs[_ppcs['quantity'].isin(['w_rep', 'ell_rep', 'vol_rep', 'alpha_
         axes[g[0]].plot(d['growth_rate_hr'], d['lower'], lw=1,
                         color=cor[f'{perc_colors[g[1]]}_black'], alpha=0.25)
 plt.tight_layout()
-# plt.savefig('../../figures/Fig3_size_ppc.pdf')
+plt.savefig('../../figures/Fig3_size_ppc.pdf')
 # %%
 fig, ax = plt.subplots(1, 2, figsize=(6, 1.5), sharex=True)
 for a in ax:
@@ -184,9 +137,7 @@ for g, d in ms_data[ms_data['localization'].isin(['periplasm', 'membrane'])].gro
                color=mapper[g]['c'], markeredgewidth=0.5, markeredgecolor=cor['primary_black'],
                alpha=0.5, ms=5)
 
-_ppcs = ppcs[(ppcs['volume_scale'] == 'linear_width') &
-             (ppcs['model'] == 'const_phi_mem')]
-for g, d in _ppcs[_ppcs['interval'].isin(perc_colors.keys()) & _ppcs['quantity'].isin(['rel_phi_rep'])].groupby(['interval'], sort=False):
+for g, d in m1_ppcs[m1_ppcs['interval'].isin(perc_colors.keys()) & m1_ppcs['quantity'].isin(['rel_phi_rep'])].groupby(['interval'], sort=False):
     if g[0] != 'median':
         ax[0].fill_between(d['width'], d['lower'], d['upper'],
                            alpha=0.3, color=cor[f'{perc_colors[g]}_blue'])
@@ -194,9 +145,8 @@ for g, d in _ppcs[_ppcs['interval'].isin(perc_colors.keys()) & _ppcs['quantity']
         ax[0].plot(d['width'], d['lower'], d['upper'],
                    alpha=0.3, color=cor[f'{perc_colors[g]}_blue'])
 
-_ppcs = ppcs[(ppcs['volume_scale'] == 'linear_width') &
-             (ppcs['model'] == 'const_rho_mem')]
-for g, d in _ppcs[_ppcs['interval'].isin(perc_colors.keys()) & _ppcs['quantity'].isin(['rel_phi_rep'])].groupby(['interval'], sort=False):
+
+for g, d in m2_ppcs[m2_ppcs['interval'].isin(perc_colors.keys()) & m2_ppcs['quantity'].isin(['rel_phi_rep'])].groupby(['interval'], sort=False):
     if g[0] != 'median':
         ax[1].fill_between(d['width'], d['lower'], d['upper'],
                            alpha=0.3, color=cor[f'{perc_colors[g]}_green'])
@@ -204,7 +154,7 @@ for g, d in _ppcs[_ppcs['interval'].isin(perc_colors.keys()) & _ppcs['quantity']
         ax[1].plot(d['width'], d['lower'], d['upper'],
                    alpha=0.3, color=cor[f'{perc_colors[g]}_green'])
 
-# plt.savefig('../../figures/Fig3_model_comparison.pdf')
+plt.savefig('../../figures/Fig3_model_comparison.pdf')
 # %%
 fig, ax = plt.subplots(2, 1, figsize=(2, 2.5), sharex=True)
 ax[0].set_ylabel(r'$\rho_{mem}$ [fg / µm$^2$]', fontsize=6)
