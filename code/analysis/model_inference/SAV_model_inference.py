90553946838ef0b0c1d40b41f81fc1f9ee6b773e
gchure <griffinchure@gmail.com>
2023-08-30 14:26:25 -0700

    Adding new inferrential model and script to process it

--- code/analysis/model_inference/SAV_model_inference.py
+++ code/analysis/model_inference/SAV_model_inference.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import cmdstanpy
 import arviz as az
 import size.viz
+import tqdm
 cor, pal = size.viz.matplotlib_style()
 model = cmdstanpy.CmdStanModel(stan_file='./SAV_model_inference.stan')
 
@@ -27,14 +28,17 @@ density_data = pd.read_csv(
 
 
 N_pred = 300
-pred_phiRb = np.linspace(0.05, 0.30, N_pred)
+phiRb_range = np.linspace(0.05, 0.30, N_pred)
+lam_range = np.linspace(0, 2.5, N_pred)
 data_dict = {
     'N_pred': N_pred,
-    'pred_phiRb': pred_phiRb,
+    'pred_phiRb_range': phiRb_range,
+    'pred_lam_range': lam_range,
 
     'N_size': len(size_data),
     'size_lam': size_data['growth_rate_hr'].values,
     'surface_areas': size_data['surface_area_um2'].values,
+    'aspect_ratios': size_data['length_um'].values / size_data['width_um'].values,
 
     'N_prot': len(prot_data),
     'prot_lam': prot_data['growth_rate_hr'].values,
@@ -57,13 +61,150 @@ data_dict = {
 _samples = model.sample(data=data_dict)
 samples = az.from_cmdstanpy(_samples)
 
+
+# %%
+# Summaries the parameter percentiles
+pars = ['rho_mem_mu', 'phi_mem_mu', 'm_peri', 'drymass_mu', 'alpha_mu']
+full_post = samples.posterior[pars].to_dataframe().reset_index()
+full_post['idx'] = 1
+full_post[pars].to_csv(
+    '../../../data/mcmc/theory_parameter_posterior_samples.csv', index=False)
+
+# Compute the parameter percentiles
+lowers = np.array([2.5, 12.5, 62.5, 50])
+uppers = 100 - lowers
+labels = ['95%', '75%', '25%', 'median']
+perc = size.viz.compute_percentiles(
+    full_post, pars, 'idx', lower_bounds=lowers, upper_bounds=uppers, interval_labels=labels)
+
+perc[['upper', 'lower', 'quantity', 'interval']].to_csv(
+    '../../../data/mcmc/theory_parameter_summaries_longform.csv', index=False)
+
+wide_df = pd.DataFrame([])
+for g, d in perc.groupby(['quantity']):
+    med = d[d['interval'] == 'median']['lower'].values[0]
+    perc = d[d['interval'] == '95%']
+    _df = pd.DataFrame({'quantity': g,
+                        'median_value': med,
+                        '97.5%': perc['lower'].values[0],
+                        '2.5%': perc['upper'].values[0]}, index=[0])
+    wide_df = pd.concat([wide_df, _df], sort=False)
+wide_df.to_csv(
+    '../../../data/mcmc/theory_parameter_summaries_wide.csv', index=False)
+
+# %%
+# Summarize the predicted growth-rate dependencies
+pars = ['pred_lam_prot', 'volume_pred', 'length_pred', 'aspect_ratio_pred',
+        'm_peri_pred', 'rho_mem_pred', 'phi_mem_pred', 'phi_peri_pred']
+lam_par_percs = pd.DataFrame([])
+for i, p in enumerate(tqdm.tqdm(pars)):
+    post = samples.posterior[p].to_dataframe().reset_index()
+    for j, lam in enumerate(lam_range):
+        post.loc[post[f'{p}_dim_0'] == j, 'growth_rate_hr'] = lam
+    percs = size.viz.compute_percentiles(post, p, 'growth_rate_hr',
+                                         lower_bounds=lowers,
+                                         upper_bounds=uppers,
+                                         interval_labels=labels)
+    lam_par_percs = pd.concat([lam_par_percs, percs], sort=False)
+lam_par_percs.to_csv(
+    '../../../data/mcmc/theory_growth_rate_prediction_summaries.csv', index=False)
+
+# %%
+# Summarize theory prediction
+pars = ['SAV_theory', 'width_theory']
+theo_par_percs = pd.DataFrame([])
+for i, p in enumerate(tqdm.tqdm(pars)):
+    post = samples.posterior[p].to_dataframe().reset_index()
+    for j, phiRb in enumerate(phiRb_range):
+        post.loc[post[f'{p}_dim_0'] == j, 'phiRb'] = phiRb
+    percs = size.viz.compute_percentiles(post, p, 'phiRb',
+                                         lower_bounds=lowers,
+                                         upper_bounds=uppers,
+                                         interval_labels=labels)
+    theo_par_percs = pd.concat([theo_par_percs, percs], sort=False)
+theo_par_percs.to_csv(
+    '../../../data/mcmc/theory_phiRb_prediction_summaries.csv', index=False)
+
+# %%
+# Summarize the empirical caluclations for the mass spectrometry data
+pars = ['ms_m_peri', 'ms_rho_mem']
+ms_par_percs = pd.DataFrame([])
+for i, p in enumerate(tqdm.tqdm(pars)):
+    post = samples.posterior[p].to_dataframe().reset_index()
+    for j, lam in enumerate(mem_data):
+        post.loc[post[f'{p}_dim_0'] == j,
+                 'source'] = mem_data['dataset_name'].values[i]
+        post.loc[post[f'{p}_dim_0'] == j,
+                 'growth_rate_hr'] = mem_data['growth_rate_hr'].values[i]
+    percs = size.viz.compute_percentiles(post, p, ['source', 'growth_rate_hr'],
+                                         lower_bounds=lowers,
+                                         upper_bounds=uppers,
+                                         interval_labels=labels)
+    ms_par_percs = pd.concat([theo_par_percs, percs], sort=False)
+ms_par_percs.to_csv(
+    '../../../data/mcmc/mass_spec_empirical_summaries_longform.csv', index=False)
+
+ms_par_wide = pd.DataFrame([])
+for g, d in ms_par_percs.groupby(['quantity', 'source', 'growth_rate_hr']):
+    med = d[d['interval'] == 'median']['lower'].values[0]
+    perc = d[d['interval'] == '95%']
+    _df = pd.DataFrame({'quantity': g[0],
+                        'source': g[1],
+                        'growth_rate_hr': g[2],
+                        'median_value': med,
+                        '97.5%': perc['lower'].values[0],
+                        '2.5%': perc['upper'].values[0]}, index=[0])
+    ms_par_wide = pd.concat([ms_par_wide, _df], sort=False)
+ms_par_wide.to_csv(
+    '../../../data/mcmc/mass_spec_empirical_summaries_wide.csv', index=False)
+
+# %%
+# Summarize the empirical caluclations for the size data
+post = samples.posterior['size_phiRb'].to_dataframe().reset_index()
+for i in range(len(size_data)):
+    post.loc[post[f'size_phiRb_dim_0'] == i,
+             'source'] = size_data['source'].values[i]
+    post.loc[post[f'size_phiRb_dim_0'] == i,
+             'growth_rate_hr'] = size_data['growth_rate_hr'].values[i]
+    post.loc[post[f'size_phiRb_dim_0'] == i,
+             'surface_to_volume'] = size_data['surface_to_volume'].values[i]
+    post.loc[post[f'size_phiRb_dim_0'] == i,
+             'width'] = size_data['width_um'].values[i]
+
+percs = size.viz.compute_percentiles(post, 'size_phiRb', ['source', 'width', 'surface_to_volume', 'growth_rate_hr'],
+                                     lower_bounds=lowers,
+                                     upper_bounds=uppers,
+                                     interval_labels=labels)
+percs.rename(columns={'size_phiRb': 'phiRb'}, inplace=True)
+
+percs.to_csv(
+    '../../../data/mcmc/size_data_empirical_summaries_longform.csv', index=False)
+
+size_par_wide = pd.DataFrame([])
+for g, d in percs.groupby(['quantity', 'source', 'growth_rate_hr', 'width', 'surface_to_volume']):
+    med = d[d['interval'] == 'median']['lower'].values[0]
+    perc = d[d['interval'] == '95%']
+    _df = pd.DataFrame({'quantity': g[0],
+                        'source': g[1],
+                        'growth_rate_hr': g[2],
+                        'width': g[3],
+                        'surface_to_volume': g[4],
+                        'median_value': med,
+                        '97.5%': perc['lower'].values[0],
+                        '2.5%': perc['upper'].values[0]}, index=[0])
+    size_par_wide = pd.concat([size_par_wide, _df], sort=False)
+size_par_wide.to_csv(
+    '../../../data/mcmc/size_data_empirical_summaries_wide.csv', index=False)
+
 # %%
 wt = pd.read_csv(
     '../../../data/mcmc/wildtype_posterior_parameter_summaries.csv')
 # %%
-fig, ax = plt.subplots(1, 1, figsize=(2, 2))
-ax.set_ylabel('surface to volume [µm$^{-1}$]', fontsize=6)
-ax.set_xlabel('ribosomal allocation', fontsize=6)
+fig, ax = plt.subplots(1, 2, figsize=(4.25, 2))
+ax[0].set_ylabel('surface to volume [µm$^{-1}$]', fontsize=6)
+ax[1].set_ylabel('average width [µm]', fontsize=6)
+for a in ax:
+    a.set_xlabel('ribosomal allocation', fontsize=6)
 labels = []
 for g, d in samples.posterior.size_phiRb.to_dataframe().reset_index().groupby('size_phiRb_dim_0'):
     src = size_data['source'].values[g]
@@ -76,38 +217,45 @@ for g, d in samples.posterior.size_phiRb.to_dataframe().reset_index().groupby('s
     fmt['label'] = lab
     med = np.median(d['size_phiRb'].values)
     perc = np.percentile(d['size_phiRb'].values, (2.5, 97.5))
-    ax.hlines(size_data['surface_to_volume'].values[g], perc[0],
-              perc[1], linewidth=1, color=cor['primary_black'])
-    ax.plot(med, size_data['surface_to_volume'].values[g], ms=4, **fmt)
-ax.plot([], [], 'o', markeredgecolor=cor['dark_blue'], markerfacecolor='w',
-        ms=4, markeredgewidth=1, label='this study')
-ax.legend(fontsize=4)
+    ax[0].hlines(size_data['surface_to_volume'].values[g], perc[0],
+                 perc[1], linewidth=1, color=cor['primary_black'])
+    ax[1].hlines(size_data['width_um'].values[g], perc[0],
+                 perc[1], linewidth=1, color=cor['primary_black'])
+    ax[0].plot(med, size_data['surface_to_volume'].values[g], ms=4, **fmt)
+    ax[1].plot(med, size_data['width_um'].values[g], ms=4, **fmt)
+ax[0].plot([], [], 'o', markeredgecolor=cor['dark_blue'], markerfacecolor='w',
+           ms=4, markeredgewidth=1, label='this study')
+ax[0].fill_between([], [], [], color=cor['pale_blue'], label='prediction')
+ax[0].legend(fontsize=4)
 lowers = np.array([2.5, 12.5, 62.5, 50])
 uppers = 100 - lowers
 labels = ['95%', '75%', '25%', 'median']
-theory_post = samples.posterior.theory.to_dataframe().reset_index()
-percs = size.viz.compute_percentiles(theory_post, 'theory', 'theory_dim_0',
-                                     lower_bounds=lowers, upper_bounds=uppers,
-                                     interval_labels=labels)
 
-int_colors = {'95%': cor['pale_blue'],
-              '75%': cor['light_blue'],
-              '25%': cor['primary_blue'],
-              'median': cor['blue']}
-for g, d in percs.groupby('interval', sort=False):
-    if g != 'median':
-        ax.fill_between(pred_phiRb, d['lower'],
-                        d['upper'], color=int_colors[g], alpha=0.5)
-    else:
-        ax.plot(pred_phiRb, d['lower'],
-                color=int_colors[g], linewidth=1, alpha=0.5)
+for i, p in enumerate(['SAV_theory',  'width_theory']):
+    theory_post = samples.posterior[p].to_dataframe().reset_index()
+    percs = size.viz.compute_percentiles(theory_post, p, f'{p}_dim_0',
+                                         lower_bounds=lowers, upper_bounds=uppers,
+                                         interval_labels=labels)
+    int_colors = {'95%': cor['pale_blue'],
+                  '75%': cor['light_blue'],
+                  '25%': cor['primary_blue'],
+                  'median': cor['blue']}
+    for g, d in percs.groupby('interval', sort=False):
+        if g != 'median':
+            ax[i].fill_between(pred_phiRb, d['lower'],
+                               d['upper'], color=int_colors[g], alpha=0.5)
+        else:
+            ax[i].plot(pred_phiRb, d['lower'],
+                       color=int_colors[g], linewidth=1, alpha=0.5)
 
 for g, d in wt.groupby('carbon_source'):
     phiRb = d[d['quantity'] == 'phi_Rb']
     sav = d[d['quantity'] == 'surface_to_volume']
-    ax.vlines(phiRb['median_value'], sav['2.5%'], sav['97.5%'],
-              linewidth=1, color=cor['blue'])
-    ax.hlines(sav['median_value'], phiRb['2.5%'], phiRb['97.5%'],
-              linewidth=1, color=cor['blue'])
-    ax.plot(phiRb['median_value'], sav['median_value'], 'o', markeredgecolor=cor['blue'],
-            markerfacecolor='w', markeredgewidth=1, ms=4)
+    width = d[d['quantity'] == 'width']
+    for i, p in enumerate([sav, width]):
+        ax[i].vlines(phiRb['median_value'], p['2.5%'], p['97.5%'],
+                     linewidth=1, color=cor['blue'])
+        ax[i].hlines(p['median_value'], phiRb['2.5%'], phiRb['97.5%'],
+                     linewidth=1, color=cor['blue'])
+        ax[i].plot(phiRb['median_value'], p['median_value'], 'o', markeredgecolor=cor['blue'],
+                   markerfacecolor='w', markeredgewidth=1, ms=4)
--- code/analysis/model_inference/SAV_model_inference.stan
+++ code/analysis/model_inference/SAV_model_inference.stan
@@ -2,7 +2,8 @@
 data {
     // Test data
     int<lower=1> N_pred;
-    vector<lower=0>[N_pred] pred_phiRb;
+    vector<lower=0>[N_pred] pred_phiRb_range;
+    vector<lower=0>[N_pred] pred_lam_range;
 
     //--------------------------------------------------------------------------
     // LITERATURE DATA
@@ -12,6 +13,7 @@ data {
     int<lower=1> N_size;
     vector<lower=0>[N_size] size_lam;
     vector<lower=0>[N_size] surface_areas;
+    vector<lower=0>[N_size] aspect_ratios;
 
     // Protein-per-cell data 
     int<lower=0> N_prot;
@@ -52,6 +54,8 @@ parameters {
     real<lower=0> surface_area_slope;
     real<lower=0> surface_area_intercept;
     real<lower=0> surface_area_sigma;
+    real<lower=0> alpha_zeroed_mu;
+    real<lower=0> alpha_sigma;
 
     real<lower=0> rho_mem_mu;
     real log_prot_intercept;
@@ -64,6 +68,7 @@ transformed parameters {
     vector<lower=0>[N_ms] ms_total_prot = exp(log_prot_intercept + log_prot_slope .* ms_lam);
     vector<lower=0>[N_ms] ms_phi_peri = m_peri ./ ms_total_prot;
     vector<lower=0>[N_ms] ms_surface_area = surface_area_intercept + surface_area_slope .* ms_lam; 
+    real<lower=1> alpha_mu = alpha_zeroed_mu + 1;
     real<lower=0> kappa = drymass_mu / rho_mem_mu;
 
 }
@@ -93,6 +98,11 @@ model {
     surface_area_sigma ~ std_normal();
     surface_areas ~ normal(surface_area_intercept + surface_area_slope .* size_lam, surface_area_sigma);
 
+    // Literature aspect ratio inference
+    alpha_zeroed_mu ~ normal(0, 3);
+    alpha_sigma ~ std_normal();
+    aspect_ratios ~ normal(alpha_mu, alpha_sigma);
+
     // Literature drymass inference
     drymass_mu ~ normal(300, 20);
     drymass_sigma ~ normal(0, 10);
@@ -108,14 +118,37 @@ model {
 
 generated quantities {
     // Calculated theory parameters
+    vector[N_pred] pred_phiRb = phiRb_intercept + phiRb_slope .* pred_lam_range;
+    vector[N_pred] pred_lam = (pred_phiRb_range - phiRb_intercept)  ./ phiRb_slope;
+    vector[N_pred] pred_phiRb_prot = exp(log_prot_intercept + log_prot_slope .* pred_lam);
+    vector[N_pred] pred_lam_prot = exp(log_prot_intercept + log_prot_slope .* pred_lam_range);
+    vector[N_pred] SAV_theory = (phi_mem_mu .* kappa) ./ (2 * (1 + (pred_phiRb_range / 0.4558) - phi_mem_mu - m_peri ./ pred_phiRb_prot)); 
+    vector[N_pred] width_theory = (24 * alpha_mu / (3 * alpha_mu - 1)) .* (1 + (pred_phiRb_range/0.4558) - phi_mem_mu - m_peri ./pred_phiRb_prot) / (kappa * phi_mem_mu);
+    vector[N_pred] width_pred = (24 * alpha_mu / (3 * alpha_mu - 1)) .* (1 + (pred_phiRb/0.4558) - phi_mem_mu - m_peri ./pred_lam_prot) / (kappa * phi_mem_mu);
+    vector[N_pred] volume_pred = (pi() / 12) * width_pred^3 * (3 * alpha_mu  - 1);
+    vector[N_pred] length_pred = alpha_mu * width_pred;
+    vector[N_pred] aspect_ratio_pred;
+    vector[N_pred] m_peri_pred;
+    vector[N_pred] rho_mem_pred;
+    vector[N_pred] phi_mem_pred;
+    vector[N_pred] phi_peri_pred;
+    for (i in 1:N_pred) {
+        aspect_ratio_pred[i] = alpha_mu;
+        m_peri_pred[i] = m_peri;
+        rho_mem_pred[i] = rho_mem_mu;
+        phi_mem_pred[i] = phi_mem_mu;
+        phi_peri_pred[i] = m_peri ./ pred_phiRb_prot[i];
+    } 
+
+    // Empirical calculations from data
     vector[N_size] size_phiRb = phiRb_intercept + phiRb_slope .* size_lam;
-    vector[N_pred] pred_lam = (pred_phiRb - phiRb_intercept)  ./ phiRb_slope;
-    vector[N_pred] pred_prot = exp(log_prot_intercept + log_prot_slope .* pred_lam);
-    vector[N_pred] theory = (phi_mem_mu .* kappa) ./ (2 * (1 + (pred_phiRb / 0.4558) - phi_mem_mu - m_peri ./ pred_prot)); 
+    vector[N_ms] ms_m_peri = ms_total_prot .* phi_peri;
+    vector[N_ms] ms_rho_mem = phi_mem .* ms_total_prot ./ (2 .* ms_surface_area);
 
     // // Modeled data posterior predictive checks
     vector[N_phiRb]  phiRb_ppc;
     vector[N_size] surface_area_ppc;
+    real aspect_ratios_ppc = normal_rng(alpha_mu, alpha_sigma);
     vector[N_prot] prot_per_cell_ppc;
     vector[N_ms] phi_mem_mu_ppc;
     vector[N_ms] phi_mem_density_ppc;
