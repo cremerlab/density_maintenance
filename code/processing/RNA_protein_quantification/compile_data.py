2fb8426d94cb49eef300a06348c80b79cf388b22
gchure <griffinchure@gmail.com>
2025-01-09 18:10:13 -0800

    Included growth rates for RNA protein

--- code/processing/RNA_protein_quantification/compile_data.py
+++ code/processing/RNA_protein_quantification/compile_data.py
@@ -1,10 +1,20 @@
 #%%
+
+import numpy as np 
+from collections import defaultdict
 import scipy.stats
 import pandas as pd 
 
-# Load the two data files
+# Load the data files
 tot_rna = pd.read_csv('./raw/total_rna.csv')
 tot_prot = pd.read_csv('./raw/total_protein_biuret.csv')
+growth_curves = pd.read_csv('./raw/growth_curves.csv')
+
+# Compute the growth rate from the growth curves 
+rates = defaultdict(list)
+for g, d in growth_curves.groupby(['carbon_source', 'replicate']):
+    popt = scipy.stats.linregress(d['elapsed_time_hr'], np.log(d['od_600nm']))
+    rates[g[0]].append(popt[0])
 
 # Apply the conversion of ug_rna_per_biomass
 tot_rna['ug_rna_per_biomass'] = tot_rna['od260nm'] * 31 / tot_rna['adjusted_od600nm']
@@ -23,6 +33,12 @@ tot_prot = tot_prot[['strain', 'carbon_source', 'replicate', 'ug_protein_per_bio
 
 # Merge together
 merged = tot_rna.merge(tot_prot, on=['strain', 'carbon_source', 'replicate'], how='inner')
+
+# Update growth rates. 
+for k, v in rates.items():
+    for i, lam in enumerate(v):
+        merged.loc[(merged['carbon_source'] == k) & (merged['replicate'] == (i+1)),
+                   'growth_rate_hr'] = lam
 merged.to_csv('./rna_protein_per_biomass.csv', index=False)
 
 
--- code/processing/collation/collate_RNA_protein_per_cell.py
+++ code/processing/collation/collate_RNA_protein_per_cell.py
@@ -1,15 +1,7 @@
 #%%
 import numpy as np
+
 import pandas as pd
-
-# Load in the growth rate measurements from the mass spectrometry samples.
-growth_rates = pd.read_csv('../mass_spectrometry/compiled_data/aggregated_growth_measurements.csv')
-growth_rates = growth_rates[growth_rates['strain']=='wildtype']
-growth_rates = growth_rates.groupby('carbon_source')['growth_rate_hr'].mean().reset_index()
- 
-# Create a hashmap
-mapper = {g[0]:g[1] for g, _ in growth_rates.groupby(['carbon_source', 'growth_rate_hr'])}
-
 
 # Load in the total RNA + protein data 
 rna_prot = pd.read_csv('../RNA_protein_quantification/rna_protein_per_biomass.csv')
@@ -17,36 +9,13 @@ rna_prot = pd.read_csv('../RNA_protein_quantification/rna_protein_per_biomass.cs
 # Load in the cells per biomass counts 
 counts = pd.read_csv('../flow_cytometry/cells_per_biomass.csv')
 
-# Add in the growth rates to the datasets. 
-for k, v in mapper.items():
-    for d in [rna_prot, counts]:
-        d.loc[d['carbon_source']==k, 'growth_rate_hr'] = v
-
-# Save all to disk before merger and aggregation
-rna_prot.to_csv('../../../data/collated/experimental_rna_protein_data.csv', index=False)
-counts.to_csv('../../../data/collated/experimental_cells_per_biomass_data.csv', index=False)
-
 # Compute the mean and std for the cells per biomass
 agged_counts = counts.groupby(['carbon_source', 'growth_rate_hr'])['cells_per_biomass'].agg(('mean', 'std')).reset_index()
+mapper = {g[0]:g[1] for g, _ in agged_counts.groupby(['carbon_source', 'mean'])}
+for k, v in mapper.items():
+    rna_prot.loc[rna_prot['carbon_source'] == k, 'mean_cells_per_biomass'] = v
 
-# Compute the mean and std for each quantity separately 
-agged_rna = rna_prot.groupby(['carbon_source', 'growth_rate_hr'])['ug_rna_per_biomass'].agg(('mean', 'std')).reset_index()
-agged_prot = rna_prot.groupby(['carbon_source', 'growth_rate_hr'])['ug_protein_per_biomass'].agg(('mean', 'std')).reset_index()
-
-# Include the new means
-agged_rna['fg_rna_per_cell_mu'] = 1E9 * agged_rna['mean'] / agged_counts['mean']
-agged_prot['fg_protein_per_cell_mu'] = 1E9 * agged_prot['mean'] / agged_counts['mean']
-
-# Propagate the errors
-agged_rna['fg_rna_per_cell_sigma'] = np.sqrt((1E9 * agged_rna['std']/agged_counts['mean'])**2 +\
-                                              (1E9 * agged_rna['mean'] * agged_counts['std'])**2/(agged_counts['mean']**4))
-agged_rna.drop(columns=['mean', 'std'], inplace=True)
-agged_prot['fg_protein_per_cell_sigma'] = np.sqrt((1E9 * agged_prot['std']/agged_counts['mean'])**2 +\
-                                              (1E9 * agged_prot['mean'] * agged_counts['std'])**2/(agged_counts['mean']**4))
-agged_prot.drop(columns=['mean', 'std'], inplace=True)
-
-# Merge and save
-merged  = agged_rna.merge(agged_prot, on=['carbon_source', 'growth_rate_hr'], how='inner')
-merged.to_csv('../../../data/collated/experimental_rna_protein_per_cell.csv', index=False)
-
-
+rna_prot['fg_rna_per_cell'] = rna_prot['ug_rna_per_biomass'] * 1E9 / rna_prot['mean_cells_per_biomass']
+rna_prot['fg_protein_per_cell'] = rna_prot['ug_protein_per_biomass'] * 1E9 / rna_prot['mean_cells_per_biomass']
+rna_prot = rna_prot[['strain', 'carbon_source', 'growth_rate_hr', 'fg_rna_per_cell', 'fg_protein_per_cell']]
+rna_prot.to_csv('../../../data/collated/experimental_rna_protein_per_cell.csv', index=False)
