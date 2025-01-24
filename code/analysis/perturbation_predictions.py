64ef920652f0a0294b935cbdc5aee482e5a832d0
gchure <griffinchure@gmail.com>
2025-01-24 14:55:05 -0800

    Reprocessed perturbation predictions

--- code/analysis/perturbation_predictions.py
+++ code/analysis/perturbation_predictions.py
@@ -8,14 +8,26 @@ data = data[data['strain']!='wildtype']
 
 # Load the parameter summaries for the density ratio from the mcmc. 
 kappa = pd.read_csv('../../data/mcmc/theory_inference_parameter_summaries.csv')
+kappa = kappa[kappa['quantity']=='kappa']
+
+
+
+data = data[['replicate', 'strain', 'carbon_source', 'inducer_conc', 'surface_to_volume_inv_um', 'phi_mem', 'phi_peri', 'phi_rib']]
+data.rename(columns={'surface_to_volume_inv_um':'measured_SAV'}, inplace=True)
 
 # Define a function to compute the theory
-def prediction(phi_mem: np.ndarray,
-               phi_peri: np.ndarray, 
-               phi_rib: np.ndarray, 
-               kappa : float,
+def prediction(kappa: float,
                beta: float = 1/0.4558) -> np.ndarray:
-    numer = kappa * phi_mem
-    denom = 2 * (1 + beta * phi_rib - phi_peri - phi_mem)
-    return numer/denom
+    numer = kappa * data.phi_mem.values
+    denom = 2 * (1 + beta * data.phi_rib.values - data.phi_peri.values - data.phi_mem.values)
+    sav = numer / denom
+    return sav
+
+
+data['predicted_SAV_mean'] = prediction(kappa['mean'].values[0])
+data['predicted_SAV_2sig_lower'] = prediction(kappa['2sig_lower'].values[0])
+data['predicted_SAV_2sig_upper'] = prediction(kappa['2sig_upper'].values[0])
+data['predicted_SAV_1sig_lower'] = prediction(kappa['1sig_lower'].values[0])
+data['predicted_SAV_1sig_upper'] = prediction(kappa['1sig_upper'].values[0])
+data.to_csv('../../data/mcmc/perturbation_predicted_SAV_summary.csv', index=False)
 
