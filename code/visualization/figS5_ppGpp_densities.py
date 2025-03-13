7f0f8b19fb43ab24f45197a3ce7eeb0b2df0554c
gchure <griffinchure@gmail.com>
2025-03-13 15:42:21 -0700

    Merged SI figure and tweaked model fig.

--- code/visualization/figS5_ppGpp_densities.py
+++
@@ -1,8 +0,0 @@
-#%%
-import pandas as pd
-import matplotlib.pyplot as plt
-import size.viz 
-cor, pal = size.viz.matplotlib_style()
-
-data = pd.read_csv('../../data/collated/aggregated_experimental_data.csv')
-data.head()
--- code/visualization/figS5_sav_lambda.py
+++ code/visualization/figS5_sav_lambda.py
@@ -8,14 +8,24 @@ cor, pal = size.viz.matplotlib_style()
 # Load 
 data = pd.read_csv('../../data/collated/aggregated_experimental_data.csv')
 
+# Directly calculate the emprical kappa given by Eq. 8 in main text
+W_PERI = 0.025
+BETA = 1 / 0.4558
+data['empirical_kappa'] = (2 * data['surface_area_um2'] / (data['volume_fL'] - W_PERI * data['surface_area_um2'])) * (data['phi_cyto'] + BETA * data['phi_rib']) / data['phi_mem']
+
+#%% Plot the break of the sav and growth rate
 # Set figure canvas
-fig, ax = plt.subplots(1, 1, figsize=(2, 2))
+fig, ax = plt.subplots(1, 2, figsize=(4, 1.75))
 
 # Plot the wildtype data
 wt = data[data['strain']=='wildtype']
-ax.plot(wt['growth_rate_hr'], wt['surface_to_volume_inv_um'], 'o',
+ax[0].plot(wt['growth_rate_hr'], wt['surface_to_volume_inv_um'], 'o',
         markerfacecolor=cor['pale_black'], ms=4,
         markeredgecolor=cor['pale_black'], alpha=0.5)
+ax[1].plot(wt['growth_rate_hr'], wt['empirical_kappa'], 'o',
+        markerfacecolor=cor['pale_black'], ms=4,
+        markeredgecolor=cor['pale_black'], alpha=0.5)
+
 
 # Define colors and markers for the perturbations.
 inducer_concs = {0:'pale_', 2:'primary_', 4:'', 100:''}
@@ -37,11 +47,21 @@ for g, d in data[data['strain']!='wildtype'].groupby(['carbon_source', 'strain',
            'markersize':4,
            'linestyle':'none',
            'zorder':zorder}
-    ax.plot(d['growth_rate_hr'], d['surface_to_volume_inv_um'],
+    ax[0].plot(d['growth_rate_hr'], d['surface_to_volume_inv_um'],
             **fmt)
+    ax[1].plot(d['growth_rate_hr'], d['empirical_kappa'],
+            **fmt)
+# Add
 # Add context
-ax.set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
-ax.set_ylabel('$S_A/V$\nsurface-to-volume [µm$^{-1}$]', fontsize=6)
-ax.set_xlim([0.25, 1.5])
-ax.set_ylim([4.5, 8])
+ax[0].set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax[1].set_xlabel('growth rate [hr$^{-1}$]', fontsize=6)
+ax[0].set_ylabel('$S_A/V$\nsurface-to-volume [µm$^{-1}$]', fontsize=6)
+ax[1].set_ylabel('cytoplasm-membrane\ndensity ratio [µm$^{-1}$]', fontsize=6)
+ax[0].set_xlim([0.25, 1.5])
+ax[1].set_xlim([0.25, 1.5])
+ax[0].set_ylim([4.5, 8])
+ax[1].set_ylim([50, 250])
 plt.savefig('./plots/figS5_sav_lambda_plot.pdf', bbox_inches='tight')
+
+
+
